4b2a28454803a4b0c962ec91b4228550b847ae95
danicheg <e.danicheg@yandex.ru>
2021-09-06 13:30:34 +0300

    Use opaque type for Any

---
+++ core/src/main/scala-3.x/src/main/scala/io.chrisdavenport.monoids/Any.scala
@@ -0,0 +1,10 @@
+package io.chrisdavenport.monoids
+
+opaque type Any = Boolean
+
+object Any extends AnyInstances:
+  def apply(getAny: Boolean): Any = getAny
+  def unapply(arg: Any): Option[Boolean] = Some(arg.getAny)
+
+extension (a: Any)
+  def getAny: Boolean = a
--- core/src/main/scala/io/chrisdavenport/monoids/AnyInstances.scala
+++ core/src/main/scala/io/chrisdavenport/monoids/AnyInstances.scala
@@ -2,17 +2,15 @@ package io.chrisdavenport.monoids
 
 import cats._
 import cats.kernel.BoundedSemilattice
-import cats.implicits._
+import cats.syntax.all._
 
-final case class Any(getAny: Boolean) extends AnyVal
-object Any {
-
+private[monoids] trait AnyInstances {
   def any[F[_]: Foldable](fa: F[Any]): Any = Any(fa.exists(_.getAny))
 
   implicit val anyMonoid: BoundedSemilattice[Any] = new BoundedSemilattice[Any] {
     val empty: Any = Any(false)
     def combine(x: Any, y: Any): Any = Any(x.getAny || y.getAny)
   }
-  implicit val anyShow: Show[Any] = Show.fromToString
+  implicit val anyShow: Show[Any] = Show.show[Any](any => s"Any(${any.getAny.show})")
   implicit val anyOrd: Order[Any] = Order.by(_.getAny)
 }
