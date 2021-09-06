66a4a66b0e15e0b9d25e8c5aacd007e819fc331d
danicheg <e.danicheg@yandex.ru>
2021-09-06 13:35:57 +0300

    Use opaque type for All

---
+++ core/src/main/scala-3.x/src/main/scala/io.chrisdavenport.monoids/All.scala
@@ -0,0 +1,10 @@
+package io.chrisdavenport.monoids
+
+opaque type All = Boolean
+
+object All extends AllInstances:
+  def apply(getAll: Boolean): All = getAll
+  def unapply(arg: All): Option[Boolean] = Some(arg.getAll)
+
+extension (a: All)
+  def getAll: Boolean = a
--- core/src/main/scala/io/chrisdavenport/monoids/AllInstances.scala
+++ core/src/main/scala/io/chrisdavenport/monoids/AllInstances.scala
@@ -2,10 +2,9 @@ package io.chrisdavenport.monoids
 
 import cats._
 import cats.kernel.BoundedSemilattice
-import cats.implicits._
+import cats.syntax.all._
 
-final case class All(getAll: Boolean) extends AnyVal
-object All {
+private[monoids] trait AllInstances {
   def all[F[_]: Foldable](fa: F[All]): All = All(fa.forall(_.getAll))
 
   implicit val allMonoid: BoundedSemilattice[All] = new BoundedSemilattice[All] {
@@ -13,6 +12,6 @@ object All {
     def combine(x: All, y: All): All = All(x.getAll && y.getAll)
   }
 
-  implicit val allShow: Show[All] = Show.fromToString
+  implicit val allShow: Show[All] = Show.show[All](all => s"All(${all.getAll.show})")
   implicit val allOrd: Order[All] = Order.by(_.getAll)
 }
